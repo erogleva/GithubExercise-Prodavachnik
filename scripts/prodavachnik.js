3ba75dcdfb4f25f36e09ac5e378b0c967c0656c6
Keskinev87 <yavor_keskinev@abv.bg>
2017-06-16 11:08:23 +0300

    Commit 3.2 Yavor

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -7,10 +7,17 @@ function startApp() {
     $("#linkHome").click(showHomeView);
     $("#linkLogin").click(showLoginView);
     $("#linkRegister").click(showRegisterView);
+	$("#linkListAds").click(listAdverts);
 	$("#linkLogout").click(logoutUser);
 	
 	$("#buttonLoginUser").click(loginUser);
     $("#buttonRegisterUser").click(registerUser);
+	
+	const kinveyBaseUrl = "https://mock.api.com/";
+    const kinveyAppKey = "kid_rk";
+    const kinveyAppSecret = "736804a668";
+
+
 
 
     function showView(viewName) {
@@ -25,11 +32,13 @@ function startApp() {
             // No logged in user
             $("#linkLogin").show();
             $("#linkRegister").show();
+			$("#linkListAds").hide();
             $("#linkLogout").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
+			$("#linkListAds").show();
             $("#linkLogout").show();
         }
     }
@@ -52,9 +61,9 @@ function startApp() {
 
 // user/login
     function loginUser() {
-        const kinveyLoginUrl = "https://mock.backend.com/user/kid_rk/login";
+        const kinveyLoginUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/login";
         const kinveyAuthHeaders = {
-            'Authorization': "Basic " + btoa("kid_rk:736804a668"),
+            'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),
         };
         let userData = {
             username: $('#formLogin input[name=username]').val(),
@@ -72,7 +81,7 @@ function startApp() {
         function loginSuccess(userInfo) {
             saveAuthInSession(userInfo);
             showHideMenuLinks();
-            showHomeView();
+            listAdverts();
         }
     }
 
@@ -84,9 +93,9 @@ function startApp() {
     }
 // user/register
     function registerUser() {
-        const kinveyRegisterUrl = "https://mock.backend.com/user/kid_rk/";
+        const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
         const kinveyAuthHeaders = {
-            'Authorization': "Basic " + btoa("kid_rk:736804a668"),
+            'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),
         };
 
         let userData = {
@@ -106,7 +115,7 @@ function startApp() {
             console.log(userInfo);
             saveAuthInSession(userInfo);
             showHideMenuLinks();
-            showHomeView();
+            listAdverts();
         }
     }
 
@@ -119,3 +128,45 @@ function startApp() {
     }
 }
 
+// advertisement/all
+    function listAdverts() {
+        $('#ads').empty();
+        showView('viewAds');
+
+        const kinveyAdvertsUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/adverts";
+        const kinveyAuthHeaders = {
+            'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
+        };
+        $.ajax({
+            method: "GET",
+            url: kinveyAdvertsUrl,
+            headers: kinveyAuthHeaders,
+            success: loadAdvertsSuccess
+        });
+
+        function loadAdvertsSuccess(adverts) {
+            if (adverts.length === 0) {
+                $('#ads').text('No advertisements available.');
+            } else {
+                let advertsTable = $('<table>')
+                    .append($('<tr>').append(
+                        '<th>Title</th>',
+                        '<th>Publisher</th>',
+                        '<th>Date Published</th>',
+                        '<th>Price</th>')
+                    );
+
+                for (let advert of adverts) {
+                    advertsTable.append($('<tr>').append(
+                        $('<td>').text(advert.title),
+                        $('<td>').text(advert.publisher),
+                        $('<td>').text(advert.datePublished),
+                        $('<td>').text(advert.price)
+                    ));
+                }
+
+                $('#ads').append(advertsTable);
+            }
+        }
+    }
+}
